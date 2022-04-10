97574d7093de1dba119cad892224b79dcf0b7b8b
Maxwuell Junio <max.junio10@gmail.com>
2022-04-10 17:32:44 -0300

    isAdjacente

--- trab_algoritmos_em_grafos/src/trab_algoritmos_em_grafos/Grafo.java
+++ trab_algoritmos_em_grafos/src/trab_algoritmos_em_grafos/Grafo.java
@@ -3,40 +3,51 @@ package trab_algoritmos_em_grafos;
 import java.util.ArrayList;
 
 public class Grafo<TIPO> {
-    
+
     private ArrayList<Vertice<TIPO>> vertices;
     private ArrayList<Aresta<TIPO>> arestas;
 
-    public Grafo(){
+    public Grafo() {
         this.vertices = new ArrayList<Vertice<TIPO>>();
         this.arestas = new ArrayList<Aresta<TIPO>>();
     }
 
-    public void adicionarVertice(TIPO dado){
+    public void adicionarVertice(TIPO dado) {
         Vertice<TIPO> novoVertice = new Vertice<TIPO>(dado);
         this.vertices.add(novoVertice);
     }
 
-    public void adicionarAresta(Double peso, TIPO dadoInicio, TIPO dadoFim){
-        //Busca os vértices pelos dados de início e fim informados
+    public void adicionarAresta(Double peso, TIPO dadoInicio, TIPO dadoFim) {
+        // Busca os vértices pelos dados de início e fim informados
         Vertice<TIPO> inicio = this.getVertice(dadoInicio);
         Vertice<TIPO> fim = this.getVertice(dadoFim);
-        //Cria uma aresta utilizando os vértices encontrados
+        // Cria uma aresta utilizando os vértices encontrados
         Aresta<TIPO> aresta = new Aresta<TIPO>(peso, inicio, fim);
         inicio.adicionarArestaSaida(aresta);
         fim.adicionarArestaEntrada(aresta);
         this.arestas.add(aresta);
     }
 
-    public Vertice<TIPO> getVertice(TIPO dado){
+    public Vertice<TIPO> getVertice(TIPO dado) {
         Vertice<TIPO> vertice = null;
-        for(int i=0; i<this.vertices.size();i++){
-            if(this.vertices.get(i).getDado().equals(dado)){
+        for (int i = 0; i < this.vertices.size(); i++) {
+            if (this.vertices.get(i).getDado().equals(dado)) {
                 vertice = this.vertices.get(i);
                 break;
             }
         }
         return vertice;
+    }
+
+    public boolean isAdjacente(Vertice<TIPO> v1, Vertice<TIPO> v2) {
+        boolean adjacente = false;
+        for (int i = 0; i < ((ArrayList<Vertice<TIPO>>) v1.getArestasSaida()).size(); i++) {
+            if (((ArrayList<Vertice<TIPO>>) v1.getArestasSaida()).get(i).getFim().equals(v2)) {
+                adjacente = true;
+                break;
+            }
+        }
+        return adjacente;
     }
 
 }
--- trab_algoritmos_em_grafos/src/trab_algoritmos_em_grafos/Vertice.java
+++ trab_algoritmos_em_grafos/src/trab_algoritmos_em_grafos/Vertice.java
@@ -3,34 +3,41 @@ package trab_algoritmos_em_grafos;
 import java.util.ArrayList;
 
 public class Vertice<TIPO> {
-    
+
     private TIPO dado;
     private ArrayList<Aresta<TIPO>> arestasEntrada;
     private ArrayList<Aresta<TIPO>> arestasSaida;
 
-    public Vertice(TIPO dado){
+    public Vertice(TIPO dado) {
         this.dado = dado;
         this.arestasEntrada = new ArrayList<Aresta<TIPO>>();
         this.arestasSaida = new ArrayList<Aresta<TIPO>>();
     }
 
-    //Getters e Setters
-    public TIPO getDado(){
+    // Getters e Setters
+    public TIPO getDado() {
         return dado;
     }
 
-    public void setDado(TIPO dado){
+    public void setDado(TIPO dado) {
         this.dado = dado;
     }
 
-    //Adicionar arestas de entrada e saída 
-    public void adicionarArestaEntrada(Aresta<TIPO> aresta){
+    // Adicionar arestas de entrada e saída
+    public void adicionarArestaEntrada(Aresta<TIPO> aresta) {
         this.arestasEntrada.add(aresta);
-    } 
+    }
 
-    public void adicionarArestaSaida(Aresta<TIPO> aresta){
+    public void adicionarArestaSaida(Aresta<TIPO> aresta) {
         this.arestasSaida.add(aresta);
     }
-    
+
+    public Object getArestasSaida() {
+        return null;
+    }
+
+    public Object getFim() {
+        return null;
+    }
 
 }
