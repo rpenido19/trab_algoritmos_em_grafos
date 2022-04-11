2db2fcb2e67b2ba6c69d9812e2b18ac9f482b06d
Gustavo Soares <gustavohvs.dev@gmail.com>
2022-04-11 16:52:42 -0300

    criação de testes pelo menu dos metódos já existentes

--- trab_algoritmos_em_grafos/src/trab_algoritmos_em_grafos/Menu.java
+++ trab_algoritmos_em_grafos/src/trab_algoritmos_em_grafos/Menu.java
@@ -89,12 +89,18 @@ public class Menu {
 		Scanner input = new Scanner(System.in);
 		String opt = new String();
 		String pause = new String();
+		int vertice1;
+		int vertice2;
 
 		System.out.println("\nAperte enter para continuar... ");
 		pause = input.nextLine();
 
 		System.out.println("Menu de opções");
 		System.out.println("_______________________________________________________________________");
+		System.out.println("getGrau				retorna o grau de um vértice");
+		System.out.println("isAdjacente			verifica dois angulos são adjacentes");
+		System.out.println("isIsolado			verifica se o vértice está isolado");
+		System.out.println("isPendente			verifica se o vértice é pendente");
 		System.out.println("isRegular			verifica se o grafo é regular");
 		System.out.println("isNulo				verifica se o grafo é nulo");
 		System.out.println("quit				finaliza o programa");
@@ -103,6 +109,36 @@ public class Menu {
 		opt = input.nextLine();
 
 		switch (opt) {
+		case "isAdjacente":
+			System.out.print("Digite o primeiro vértice: ");
+			vertice1 = input.nextInt();
+			System.out.print("Digite o segundo vértice: ");
+			vertice2 = input.nextInt();
+			grafo.isAdjacente(vertice1, vertice2);
+			menu(grafo, verticesGrafo);
+			break;
+		case "getGrau":
+			System.out.print("Digite do vértice: ");
+			vertice1 = input.nextInt();
+			grafo.getGrau(vertice1);
+			menu(grafo, verticesGrafo);
+			break;
+		case "isIsolado":
+			System.out.print("Digite do vértice: ");
+			vertice1 = input.nextInt();
+			grafo.isIsolado(vertice1);
+			menu(grafo, verticesGrafo);
+			break;
+		case "isPendente":
+			System.out.print("Digite do vértice: ");
+			vertice1 = input.nextInt();
+			if(grafo.isPendente(vertice1)==true){
+				System.out.println("O vértice é pendente");
+			}else{
+				System.out.println("O vértice não é pendente");
+			}
+			menu(grafo, verticesGrafo);
+			break;
 		case "isRegular":
 			if(grafo.isRegular(verticesGrafo)==true){
 				System.out.println("O grafo é regular");
